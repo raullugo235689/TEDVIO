68a93f9703270ea0e3f39e960e2674740df3cf6b
raullugo235689 <raullugo235689@gmail.com>
2026-08-17 03:09:27 -0700

    Connect TEDVIO to Supabase

---
+++ config.js
@@ -0,0 +1,5 @@
+window.TEDVIO_CONFIG = {
+  SUPABASE_URL: "https://ggjknixnrjzkzkpwbwsl.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_2sQojzbcJWI4AXwFsNallQ_bvRE9oFc",
+  DEMO_MODE: false
+};
