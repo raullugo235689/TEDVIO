97a05ce3cdd7f84dd3c385014bf1d05ff4ec7842
raullugo235689 <raullugo235689@gmail.com>
2026-08-17 08:15:16 -0700

    Add runtime hooks for stable student polling

---
+++ beta-runtime-hooks.js
@@ -0,0 +1,17 @@
+(()=>{
+  const nativeSetInterval=window.setInterval.bind(window);
+  const nativeClearInterval=window.clearInterval.bind(window);
+  window.__TEDVIO_NATIVE_SET_INTERVAL__=nativeSetInterval;
+  window.__TEDVIO_NATIVE_CLEAR_INTERVAL__=nativeClearInterval;
+  window.__TEDVIO_INTERVALS_BY_DELAY__=window.__TEDVIO_INTERVALS_BY_DELAY__||{};
+  window.setInterval=function(fn,delay,...args){
+    const id=nativeSetInterval(fn,delay,...args);
+    const key=String(Number(delay)||0);
+    (window.__TEDVIO_INTERVALS_BY_DELAY__[key]||(window.__TEDVIO_INTERVALS_BY_DELAY__[key]=[])).push(id);
+    if(typeof fn==='function'&&fn.name==='renderStudent'){
+      window.__TEDVIO_STUDENT_RENDER__=fn;
+      window.__TEDVIO_STUDENT_INTERVAL__=id;
+    }
+    return id;
+  };
+})();
